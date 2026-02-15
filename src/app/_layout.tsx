e9a2cfcf20ac2c7e4bc105b94c9bf3122f55fc49
EngiBeerHub <engibeer0422@gmail.com>
2026-02-15 18:56:16 +0900

    add: ultracite

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,24 @@
 import "../global.css";
 
-import {Slot} from "expo-router";
-import {GestureHandlerRootView} from "react-native-gesture-handler";
-import {HeroUINativeConfig, HeroUINativeProvider} from "heroui-native";
+import { Slot } from "expo-router";
+import { type HeroUINativeConfig, HeroUINativeProvider } from "heroui-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 /**
  * HeroUI Native の開発時設定。
  */
 const config: HeroUINativeConfig = {
-    devInfo: {
-        // スタイリング指針の案内表示を無効化する
-        stylingPrinciples: false
-    }
+  devInfo: {
+    // スタイリング指針の案内表示を無効化する
+    stylingPrinciples: false,
+  },
 };
 export default function Layout() {
-    return (
-        <GestureHandlerRootView>
-            <HeroUINativeProvider config={config}>
-                <Slot/>
-            </HeroUINativeProvider>
-        </GestureHandlerRootView>
-    );
+  return (
+    <GestureHandlerRootView>
+      <HeroUINativeProvider config={config}>
+        <Slot />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
 }
